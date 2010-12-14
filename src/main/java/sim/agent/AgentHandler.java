ac962e99ca7c2b10ec55f76d045a928e4d6f7bba
Mihai Chezan <mihai.chezan@softgress.com>
2010-12-14 23:56:43 +0200

    increase buffer of maximum allowed transmited measurements from 100 to 10000

--- src/main/java/sim/agent/AgentHandler.java
+++ src/main/java/sim/agent/AgentHandler.java
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */ 
+ */
 package sim.agent;
 
 import java.io.EOFException;
@@ -23,7 +23,6 @@ import java.io.OutputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import sim.data.Collector;
 import sim.data.MethodMetrics;
 import sim.data.SystemId;
 
@@ -34,21 +33,21 @@ import com.sun.net.httpserver.HttpHandler;
  * The http handle reading method metrics objects.
  * 
  * @author valer
- *
+ * 
  */
 @SuppressWarnings("restriction")
 public class AgentHandler implements HttpHandler {
 
 	private static final Logger logger = LoggerFactory.getLogger(AgentHandler.class);
-	
+
 	private SystemId systemId;
-	
-	private static final int MAX_METRICS_READ = 100;
-	
+
+	private static final int MAX_METRICS_READ = 10000;
+
 	public AgentHandler(SystemId systemId) {
 		this.systemId = systemId;
 	}
-	
+
 	@Override
 	public void handle(HttpExchange xchg) throws IOException {
 		ObjectInputStream ois = new ObjectInputStream(xchg.getRequestBody());
@@ -57,14 +56,14 @@ public class AgentHandler implements HttpHandler {
 			int count = 0;
 			while (true) {
 				if (count == MAX_METRICS_READ) {
-					logger.info("max object read of " + MAX_METRICS_READ + " was reached, closing connection");
+					logger.warn("max object read of " + MAX_METRICS_READ + " was reached, closing connection");
 					break;
 				}
 				try {
 					o = ois.readObject();
 					count++;
 				} catch (EOFException e) {
-					logger.info("no more data to read, closing connection ...");
+					logger.debug("no more data to read, closing connection ...");
 					break;
 				}
 				if (o instanceof MethodMetrics) {
@@ -80,9 +79,9 @@ public class AgentHandler implements HttpHandler {
 		}
 
 		xchg.sendResponseHeaders(200, "SUCCESS".length());
-	    OutputStream os = xchg.getResponseBody();
-	    os.write("SUCCESS".getBytes());
-	    os.close();
+		OutputStream os = xchg.getResponseBody();
+		os.write("SUCCESS".getBytes());
+		os.close();
 	}
 
 }
